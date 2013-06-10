971fa3715a5d952e1457ea2ef3ace423b7826107
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-06-10 14:26:42 -0700

    module working, but no data inserted in it

--- db_info.rb
+++ db_info.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :host => 'localhost',
-  :username => 'elizahyde',
+  :username => 'Ykat',
   :password => '',
   :database => 'potlucks',
   :encoding => 'utf8'
